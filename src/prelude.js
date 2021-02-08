83c5229b52e5792bfcd1a80498dfcf22aab9bac2
kirjavascript <snkenjoi@gmail.com>
2021-02-08 00:44:51 +0000

    added pathfinder + SDL stuff

--- src/prelude.js
+++ src/prelude.js
@@ -3,17 +3,25 @@
  * TODO
  *
  * add object-inspect
+ * title / get_window
  */
 
 (function() {
     let id = 0;
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
 
     class HTMLCanvasElement {
         constructor() {
             this.id = id++;
         }
 
-        getContext = () => new Context(this.id);
+        getContext = (type) => {
+            if (type !== '2d') {
+                throw new Error(`Currently only 2D rendering is supported`);
+            }
+            return new CanvasRenderingContext2D(this.id);
+        };
 
         #width = 300;
         #height = 150;
@@ -30,8 +38,9 @@
         //toBlob
     }
 
-    class Context {
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 
+    class CanvasRenderingContext2D {
     }
 
     Object.assign(globalThis, {
